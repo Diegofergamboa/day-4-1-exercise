6e8c66d27d5d315e1a9e4f5124b33bad62cea28d
Diegofergamboa <diegofergamboa@hotmail.es>
2021-03-23 20:22:10 +0000

    Done challenge!

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 #Write your code below this line 👇
 #Hint: Remember to import the random module first. 🎲
 
+import random
+pc_choice = random.randint(0,1)
+
+if pc_choice == 0:
+	print('Machine says Head')
+elif pc_choice == 1:
+	print('Machine says tail')
+else:
+	print('Choose a value between 0 or 1')
 
 
 
